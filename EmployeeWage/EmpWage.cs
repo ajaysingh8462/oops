05d9cca74e94b5216a45d3f05ca569c35fce788c
ajaysingh8462 <ajaysingh84625@gmail.com>
2022-12-11 14:52:55 +0530

    [ajay]- Employee is present or absent check by random numnber

---
+++ EmployeeWage/EmpWage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    internal class EmpWage
+    {
+        public void Emp()
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            int empPresent = 1;
+            Random random = new Random();
+            int check = random.Next(0, 2);
+            if (check == empPresent)
+            {
+                Console.WriteLine("EMPLOYEE IS PRESENT");
+
+            }
+            else 
+            {
+                Console.WriteLine("EMPLOYEE IS ABSENT");
+            }
+        }
+    }
+}
---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            EmpWage wage= new EmpWage();
+            wage.Emp();
+        }
+    }
+
+}
